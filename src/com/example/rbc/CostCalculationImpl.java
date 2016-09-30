d9ca6aee0d72acc089becda65f980f9d844dae98
Amrit-Jaiswal <jaiswal.amrit@gmail.com>
2016-09-30 13:23:31 +0530

    Add files via upload

---
+++ src/com/example/rbc/CostCalculationImpl.java
@@ -0,0 +1,56 @@
+package com.example.rbc;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.example.bean.Price;
+
+public class CostCalculationImpl implements CostCalculation{
+	
+	private long totalCost =0;
+	private long itemPrice = 0;
+	
+	Price price = new Price(10,20,30,40,50);
+
+	/**
+	 * Calculate the total price of items in basket
+	 */
+	@Override
+	public long totalCost(Map<String, Integer> basket){
+		
+		if(basket != null){
+			Set<String> set = basket.keySet();
+			
+			for(String key: set){
+				if(null != key){
+					switch (key){
+						case "Bananas":
+							itemPrice = price.getBananaPrice();
+							break;
+						case "Oranges":
+							itemPrice = price.getOrangePrice();
+							break;
+						case "Apples":
+							itemPrice = price.getApplePrice();
+							break;
+						case "Lemons":
+							itemPrice = price.getLemonPrice();
+							break;
+						case "Peaches":
+							itemPrice = price.getPeachePrice();
+							break;
+					}
+					if(basket.get(key) <= 0){
+						itemPrice=0;
+					}
+					totalCost = totalCost + basket.get(key)*itemPrice;
+				}
+			}
+		}
+		else{
+			System.out.println("Basket is null");
+		}
+		return totalCost;
+	}
+
+}
---
+++ src/com/example/testcase/CostCalculationTestCase.java
@@ -0,0 +1,43 @@
+package com.example.testcase;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.example.rbc.CostCalculationImpl;
+
+public class CostCalculationTestCase {
+	
+	public Map<String, Integer> basket = new HashMap<String, Integer>();
+	
+	@Test
+	public void testTotalCost(){
+		basket.put("Bananas", 1);
+		basket.put("Oranges", 1);
+		basket.put("Apples", 1);
+		basket.put("Lemons", 1);
+		basket.put("Peaches", 1);
+		
+		//1. null value testing
+		assertEquals(0,(new CostCalculationImpl()).totalCost(null));
+		
+		//2. Empty value testing
+		assertEquals(0,(new CostCalculationImpl()).totalCost(new HashMap<String, Integer>())); 
+		
+		//3. valid values testing
+		assertEquals(150,(new CostCalculationImpl()).totalCost(basket));
+		
+		//4. Negative value testing
+		basket.put("Bananas", -1);
+		assertEquals(140,(new CostCalculationImpl()).totalCost(basket));
+		
+		//5. Zero value testing
+		basket.put("Bananas", 0);
+		assertEquals(140,(new CostCalculationImpl()).totalCost(basket));
+		
+	}
+	
+}
